20a15ac78c4e39144cc3578f31082e2c16f3bb21
Darwin <darwin78913@gmail.com>
2021-02-05 17:34:55 -0500

    shopping period update 2/5

--- public_projects/popular_courses_202101/scripts/create_tables.R
+++ public_projects/popular_courses_202101/scripts/create_tables.R
@@ -40,7 +40,7 @@ coursetable <- read_csv("../raw-data/coursetable.csv") %>%
 
 # for some reason DISR 999 gets through, manually remove it here
 top_ids <- demand %>%
-	filter(date == max(date) & id != 580) %>%
+	filter(date == max(date) & id != 583) %>%
 	arrange(desc(count)) %>%
 	top_n(10)
 
